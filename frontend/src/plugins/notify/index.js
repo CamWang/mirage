21a6c18b2d6344509b7a3c2011ea8c3054be0454
CamWang <camwang@outlook.com>
2021-05-23 22:19:10 +0800

    fix[fe]: add some comment on Notification.vue

--- frontend/src/plugins/notify/index.js
+++ frontend/src/plugins/notify/index.js
@@ -1,6 +1,28 @@
 import Notification from './components/Notification.vue'
 import { events } from './events'
 
+/**
+ * Notification Center
+ * @author CamWang
+ * 
+ * A Notification Center based on vuetify and vue-notification
+ * 
+ * Usage:
+ *  In vue instance:
+ *    this.$notify()
+ *  Outside of vue instance:
+ *    import Vue from 'vue'
+ *    Vue.notify()
+ *  Params of notyfy():
+ *    One string as param will be the notifacation text.
+ *    OR an config object
+ *      {
+ *        title:'',
+ *        text:'',
+ *        type:'' // theme color, eg. primary,error,success
+ *      }
+ */
+
 export default {
   install(Vue, options = {}) {
     if (this.installed) {
