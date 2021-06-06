0e9e9e1146616027b57872f8b156461e027de138
CamWang <camwang@outlook.com>
2021-06-06 16:43:38 +0800

    feat[fe]: little progress on rank list

--- frontend/src/components/contest/Rank.vue
+++ frontend/src/components/contest/Rank.vue
@@ -1,6 +1,32 @@
 <template>
-  <div>
-    detail
+  <div class="rank-container">
+    <div class="rank-header">
+      <div class="rank-number">Rank</div>
+      <div class="rank-profile">
+        <p class="subtitle-1">昵称</p>
+        <p class="subtitle-2">用户名</p>
+      </div>
+      <div class="rank-info">
+        <div class="rank-code">A</div>
+        <div class="rank-code">B</div>
+        <div class="rank-code">C</div>
+        <div class="rank-code">D</div>
+      </div>
+    </div>
+
+    <div class="rank-row">
+      <div class="rank-number">1</div>
+      <div class="rank-profile">
+        <p class="subtitle-1">Admin</p>
+        <p class="subtitle-2">admin</p>
+      </div>
+      <div class="rank-info">
+        <div class="rank-item"></div>
+        <div class="rank-item"></div>
+        <div class="rank-item"></div>
+        <div class="rank-item"></div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -40,6 +66,73 @@ export default {
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
+.rank-container {
+  display: flex;
+  flex-direction: column;
+  border: 1px solid #000;
 
+  p {
+    margin: 0;
+  }
+
+  .rank-header {
+    display: flex;
+    flex-direction: row;
+    border: #eee 1px solid;
+    height: 4.5rem;
+
+    .rank-number {
+      width: 3.5rem;
+      font-size: 0.75rem;
+      font-weight: 600;
+      padding: 0.5rem 0.6rem;
+      border: green 1px solid;
+    }
+  }
+
+  .rank-row {
+    display: flex;
+    flex-direction: row;
+    border: 1px solid red;
+    height: 4.5rem;
+
+    .rank-number {
+      width: 3.5rem;
+      font-size: 1rem;
+      text-align: center;
+      font-weight: 600;
+      padding: 0.5rem 0.6rem;
+      border: green 1px solid;
+    }
+  }
+
+  .rank-profile {
+    overflow: hidden;
+    width: 9rem;
+    border: 1px solid blue;
+    padding: 0.5rem 0.6rem;
+    text-align: right;
+  }
+
+  .rank-info {
+    display: flex;
+    flex-direction: row;
+
+    .rank-item {
+      width: 6rem;
+      border: 1px solid yellow; 
+      padding: 0.5rem 0.6rem;
+    }
+
+    .rank-code {
+      width: 6rem;
+      text-align: center;
+      font-size: 1.25rem;
+      font-weight: 600;
+      padding: 0.5rem 0.6rem;
+      border: 1px solid cyan;
+    }
+  }
+}
 </style>
