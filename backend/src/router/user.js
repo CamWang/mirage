2bb25e719f5c85e88cad4f108a3a3108a3ae4d9e
CamWang <camwang@outlook.com>
2021-08-06 16:31:24 +0800

    feat[be]: fetch logic change

--- backend/src/router/user.js
+++ backend/src/router/user.js
@@ -1,12 +1,20 @@
 const Router = require("@koa/router");
 const { User } = require("../model/user");
+const config = require("./config");
 
 const user = new Router();
+
+const log = global.log;
 
 user.get('/', async (ctx, next) => {
   let data = await User.find({});
   ctx.body = data;
 });
+
+user.get('/list', async (ctx, next) => {
+  let data = await User.find();
+  ctx.body = data;
+})
 
 user.post('/register', async (ctx, netx) => {
   const user = new User({
@@ -14,7 +22,9 @@ user.post('/register', async (ctx, netx) => {
     password: ctx.request.body.password
   });
   const error = user.validateSync();
-  console.log(error);
+  if (error && config.mode === "development") {
+    log.error(error);
+  }
   await user.save();
   ctx.body = "user";
 });
