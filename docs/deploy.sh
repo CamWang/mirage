614c60c630619aa284ef4bee83e7476d82ecbe6e
CamWang <camwang@outlook.com>
2021-08-12 09:59:52 +0800

    ci: doc deploy.sh

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -9,17 +9,24 @@ npm run docs:build
 # navigate into the build output directory
 cd docs/.vitepress/dist
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+if [ -z "$GITHUB_TOKEN" ]; then
+  msg='Local trigger deploy'
+  githubUrl=git@github.com:CamWang/mirage.git
+else
+  msg='Github Action trigger deploy'
+  githubUrl=https://camwang:${GITHUB_TOKEN}@github.com/CamWang/mirage.git
+  git config --global user.name "camwang"
+  git config --global user.email "camwang@outlook.com"
+fi
 
 git init
 git add -A
-git commit -m 'deploy'
+git commit -m "${msg}"
 
 # if you are deploying to https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:camwang/mirage.git master:gh-pages
+git push -f githubUrl main:gh-pages
 
 cd -
