a3b35fb9ae5bd7c914ee5d8e93f7a148f0c42937
CamWang <camwang@outlook.com>
2021-08-14 21:46:07 +0800

    chore[be]: clean console.logs

--- backend/src/router/judger.js
+++ backend/src/router/judger.js
@@ -1,35 +1,39 @@
 const Router = require("@koa/router");
+const EventEmitter = require("events").EventEmitter;
+
 const { Inferno } = require("../core/inferno");
 const config = require("../config").core;
+const { Submission } = require("../model/submission");
+const { Problem } = require("../model/problem");
 const {
   InternalServerError,
   RequestError
 } = require("../util/error");
-const { Submission } = require("../model/submission");
-const { Problem } = require("../model/problem");
+
+const emitter = new EventEmitter();
+
+emitter.on("init", () => {
+});
+emitter.on("compile", () => {
+});
+emitter.on("judge", () => {
+});
 
 const judger = new Router();
 
 function judge(task) {
-  console.log("start judge");
   let inferno;
   try {
-    inferno = new Inferno();
-    console.log("set judger");
+    inferno = new Inferno(emitter.emit.bind(emitter));
     inferno.setJudger(config.direcotry, config.ptrace, config.seccomp, config.rlimit, config.command[task.lang].source, 
       config.command[task.lang].executable, config.command[task.lang].compile, config.command[task.lang].run
     );
-    console.log("set task");
     inferno.setTask(task.id, task.tlmt, task.mlmt, task.lang, task.type, task.mode, task.data, task.spj, task.code);
-    console.log("seted task");
   } catch(err) {
     return Promise.reject(err);
   }
-  console.log("return judge");
   return new Promise((resolve, reject) => {
-    console.log("start judge");
     inferno.judgeAsync(result => {
-      delete inferno;
       resolve(result);
     });
   });
@@ -99,8 +103,7 @@ judger.post('/submit', async (ctx, next) => {
   submission.time = result.timerun;
   submission.memory = result.memory;
   await submission.save();
-
-  ctx.body = result
+  ctx.body = result;
 });
 
 module.exports = judger;
